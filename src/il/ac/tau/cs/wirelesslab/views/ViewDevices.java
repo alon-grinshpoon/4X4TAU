709bfb61b1a56c7b65b7c81bea27ee7b8277b21a
Alon Grinshpoon <alon.grinshpoon@yahoo.com>
2014-11-01 14:06:26 +0200

    Added ViewDevices

---
+++ src/il/ac/tau/cs/wirelesslab/views/ViewDevices.java
@@ -0,0 +1,28 @@
+package il.ac.tau.cs.wirelesslab.views;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.part.ViewPart;
+
+public class ViewDevices extends ViewPart {
+
+	public static final String ID = "4X4Plugin.viewPlayer";
+			
+	public ViewDevices() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void createPartControl(Composite parent) {
+		Label label = new Label(parent, SWT.None); //new up a Label widget
+		label.setText("This is the Player view.");
+		
+		CustomComposite cc = new CustomComposite(parent, SWT.None);
+	}
+
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
