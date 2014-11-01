674b6875d52789cf63d5fc1f99b8d9beceaf85df
Alon Grinshpoon <alon.grinshpoon@yahoo.com>
2014-11-01 17:52:19 +0200

    Record Composite And View

--- src/il/ac/tau/cs/wirelesslab/views/ViewRecord.java
+++ src/il/ac/tau/cs/wirelesslab/views/ViewRecord.java
@@ -1,6 +1,6 @@
 package il.ac.tau.cs.wirelesslab.views;
 
-import il.ac.tau.cs.wirelesslab.composites.CustomComposite;
+import il.ac.tau.cs.wirelesslab.composites.RecordComposite;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
@@ -9,7 +9,7 @@ import org.eclipse.ui.part.ViewPart;
 
 public class ViewRecord extends ViewPart {
 
-	public static final String ID = "4X4Plugin.viewDevices";
+	public static final String ID = "4X4Plugin.viewRecord";
 			
 	public ViewRecord() {
 		// TODO Auto-generated constructor stub
@@ -17,9 +17,9 @@ public class ViewRecord extends ViewPart {
 
 	public void createPartControl(Composite parent) {
 		Label label = new Label(parent, SWT.None); //new up a Label widget
-		label.setText("This is the Devices view.");
+		label.setText("This is the Record view.");
 		
-		CustomComposite cc = new CustomComposite(parent, SWT.None);
+		RecordComposite cc = new RecordComposite(parent, SWT.None);
 	}
 
 	public void setFocus() {
