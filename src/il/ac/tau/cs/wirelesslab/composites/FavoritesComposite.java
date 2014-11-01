99e6392759c960e8d89266d97c47f59863065b54
Alon Grinshpoon <alon.grinshpoon@yahoo.com>
2014-11-01 17:44:27 +0200

    Added Graphics

--- src/il/ac/tau/cs/wirelesslab/composites/FavoritesComposite.java
+++ src/il/ac/tau/cs/wirelesslab/composites/FavoritesComposite.java
@@ -1,9 +1,13 @@
 package il.ac.tau.cs.wirelesslab.composites;
 
 import java.io.InputStream;
+
 import il.ac.tau.cs.wirelesslab.graphics.Utils;
+
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
 
@@ -52,13 +56,77 @@ public class FavoritesComposite extends Composite {
 				false));
 
 		String file = Utils.PACKAGE_PATH + Utils.getSkin() + "/Favorites.png";
-		InputStream resource = FavoritesComposite.class.getClassLoader().getResourceAsStream(file);
-		Image img = new Image(this.getDisplay(), resource);
+		InputStream resource = FavoritesComposite.class.getClassLoader()
+				.getResourceAsStream(file);
+		final Image img = new Image(this.getDisplay(), resource);
 		favButton1.setImage(img);
 		favButton2.setImage(img);
 		favButton3.setImage(img);
 		favButton4.setImage(img);
 		favButton5.setImage(img);
+
+		// Set Buttons Text
+		favButton1.addListener(SWT.Paint, new Listener() {
+			public void handleEvent(Event event) {
+				GC gc = event.gc;
+				String text = "1";
+				Point textSize = gc.textExtent(text);
+				gc.drawText(text,
+						img.getImageData().width / 2 - textSize.x / 2,
+						img.getImageData().height / 2 - textSize.y, true);
+			}
+		});
+		// Set Buttons Text
+		favButton1.addListener(SWT.Paint, new Listener() {
+			public void handleEvent(Event event) {
+				GC gc = event.gc;
+				String text = "1";
+				Point textSize = gc.textExtent(text);
+				gc.drawText(text,
+						img.getImageData().width / 2 - textSize.x / 2,
+						img.getImageData().height / 2 - textSize.y, true);
+			}
+		});
+		favButton2.addListener(SWT.Paint, new Listener() {
+			public void handleEvent(Event event) {
+				GC gc = event.gc;
+				String text = "2";
+				Point textSize = gc.textExtent(text);
+				gc.drawText(text,
+						img.getImageData().width / 2 - textSize.x / 2,
+						img.getImageData().height / 2 - textSize.y, true);
+			}
+		});
+		favButton3.addListener(SWT.Paint, new Listener() {
+			public void handleEvent(Event event) {
+				GC gc = event.gc;
+				String text = "3";
+				Point textSize = gc.textExtent(text);
+				gc.drawText(text,
+						img.getImageData().width / 2 - textSize.x / 2,
+						img.getImageData().height / 2 - textSize.y, true);
+			}
+		});
+		favButton4.addListener(SWT.Paint, new Listener() {
+			public void handleEvent(Event event) {
+				GC gc = event.gc;
+				String text = "4";
+				Point textSize = gc.textExtent(text);
+				gc.drawText(text,
+						img.getImageData().width / 2 - textSize.x / 2,
+						img.getImageData().height / 2 - textSize.y, true);
+			}
+		});
+		favButton5.addListener(SWT.Paint, new Listener() {
+			public void handleEvent(Event event) {
+				GC gc = event.gc;
+				String text = "5";
+				Point textSize = gc.textExtent(text);
+				gc.drawText(text,
+						img.getImageData().width / 2 - textSize.x / 2,
+						img.getImageData().height / 2 - textSize.y, true);
+			}
+		});
 
 		composite = new Composite(this, SWT.NONE);
 		composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, true,
--- src/il/ac/tau/cs/wirelesslab/composites/SpectrogramComposite.java
+++ src/il/ac/tau/cs/wirelesslab/composites/SpectrogramComposite.java
@@ -1,13 +1,19 @@
 package il.ac.tau.cs.wirelesslab.composites;
 
+import java.io.InputStream;
+
 import il.ac.tau.cs.wirelesslab.graphics.Utils;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
 
 public class SpectrogramComposite extends Composite{
 	
@@ -29,8 +35,23 @@ public class SpectrogramComposite extends Composite{
 		specButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
 				false));
 
-		specButton.setText("Spectrogram");
+		// Set Button Image
+		String file = Utils.PACKAGE_PATH + Utils.getSkin() + "/SoundWave.png";
+		InputStream resource = FavoritesComposite.class.getClassLoader().getResourceAsStream(file);
+		final Image img = new Image(this.getDisplay(), resource);
+		specButton.setImage(img);
 		
+		// Set Button Text
+		specButton.addListener(SWT.Paint, new Listener() {
+	        public void handleEvent(Event event) {
+	            GC gc = event.gc;
+	            String text = "Start Spectrogram Screen";
+				Point textSize = gc.textExtent(text);
+	            gc.drawText(text, img.getImageData().width / 2 - textSize.x / 2, img.getImageData().height / 2 - textSize.y, true);
+	        }
+	    });
+
+		// Set Composites
 		composite = new Composite(this, SWT.NONE);
 		composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, true,
 				1, 1));
