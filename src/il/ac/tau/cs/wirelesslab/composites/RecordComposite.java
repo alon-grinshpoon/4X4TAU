7f8ea6ade2eefa351a32553a2656e50fac57a375
Alon Grinshpoon <alon.grinshpoon@yahoo.com>
2014-11-01 18:41:13 +0200

    Record GUI

--- src/il/ac/tau/cs/wirelesslab/composites/RecordComposite.java
+++ src/il/ac/tau/cs/wirelesslab/composites/RecordComposite.java
@@ -1,9 +1,11 @@
 package il.ac.tau.cs.wirelesslab.composites;
 
+import java.io.InputStream;
 
 import il.ac.tau.cs.wirelesslab.graphics.Utils;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -18,7 +20,7 @@ public class RecordComposite extends Composite {
 	private Button stopButton;
 
 	final static int NUM_OF_BUTTONS = 6;
-	
+
 	private Composite composite;
 
 	public RecordComposite(Composite parent, int style) {
@@ -31,12 +33,13 @@ public class RecordComposite extends Composite {
 		gridLayout.numColumns = NUM_OF_BUTTONS;
 		setLayout(gridLayout);
 
+		backwardButton = new Button(this, SWT.PUSH);
+		stopButton = new Button(this, SWT.PUSH);
+		pauseButton = new Button(this, SWT.PUSH);
 		playButton = new Button(this, SWT.PUSH);
 		recordButton = new Button(this, SWT.PUSH);
 		forwardButton = new Button(this, SWT.PUSH);
-		backwardButton = new Button(this, SWT.PUSH);
-		pauseButton = new Button(this, SWT.PUSH);
-		stopButton = new Button(this, SWT.PUSH);
+		
 
 		playButton.addMouseListener(new Utils.ExitMouseListener());
 		recordButton.addMouseListener(new Utils.ExitMouseListener());
@@ -45,18 +48,55 @@ public class RecordComposite extends Composite {
 		pauseButton.addMouseListener(new Utils.ExitMouseListener());
 		stopButton.addMouseListener(new Utils.ExitMouseListener());
 
+		backwardButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
+				false));
+		stopButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
+				false));
 		playButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
 				false));
 		recordButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
 				false));
-		forwardButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
-				false));
-		backwardButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
-				false));
 		pauseButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
 				false));
-		stopButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
+		forwardButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
 				false));
+
+		// Set Buttons Images
+		String filePlay = Utils.PACKAGE_PATH + Utils.getSkin() + "/Play.png";
+		InputStream resourcePlay = FavoritesComposite.class.getClassLoader()
+				.getResourceAsStream(filePlay);
+		final Image imgPlay = new Image(this.getDisplay(), resourcePlay);
+		playButton.setImage(imgPlay);
+		
+		String fileStop = Utils.PACKAGE_PATH + Utils.getSkin() + "/Stop.png";
+		InputStream resourceStop = FavoritesComposite.class.getClassLoader()
+				.getResourceAsStream(fileStop);
+		final Image imgStop = new Image(this.getDisplay(), resourceStop);
+		stopButton.setImage(imgStop);
+		
+		String fileRecord = Utils.PACKAGE_PATH + Utils.getSkin() + "/Record.png";
+		InputStream resourceRecord = FavoritesComposite.class.getClassLoader()
+				.getResourceAsStream(fileRecord);
+		final Image imgRecord = new Image(this.getDisplay(), resourceRecord);
+		recordButton.setImage(imgRecord);
+
+		String fileForward = Utils.PACKAGE_PATH + Utils.getSkin() + "/Forward.png";
+		InputStream resourceForward = FavoritesComposite.class.getClassLoader()
+				.getResourceAsStream(fileForward);
+		final Image imgForward = new Image(this.getDisplay(), resourceForward);
+		forwardButton.setImage(imgForward);
+		
+		String fileBackwards = Utils.PACKAGE_PATH + Utils.getSkin() + "/Backwards.png";
+		InputStream resourceBackwards = FavoritesComposite.class.getClassLoader()
+				.getResourceAsStream(fileBackwards);
+		final Image imgBackwards = new Image(this.getDisplay(), resourceBackwards);
+		backwardButton.setImage(imgBackwards);
+		
+		String filePause = Utils.PACKAGE_PATH + Utils.getSkin() + "/Pause.png";
+		InputStream resourcePause = FavoritesComposite.class.getClassLoader()
+				.getResourceAsStream(filePause);
+		final Image imgPause = new Image(this.getDisplay(), resourcePause);
+		pauseButton.setImage(imgPause);
 
 		playButton.setText("Play");
 		recordButton.setText("Record");
