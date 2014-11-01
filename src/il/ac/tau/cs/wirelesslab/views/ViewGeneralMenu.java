2d189772f36427fa620152f13e5de481957f10dc
yaelnewman <yael.newman@gmail.com>
2014-11-01 19:30:25 +0200

    views menu

---
+++ src/il/ac/tau/cs/wirelesslab/views/ViewGeneralMenu.java
@@ -0,0 +1,21 @@
+package il.ac.tau.cs.wirelesslab.views;
+
+import il.ac.tau.cs.wirelesslab.composites.CustomComposite;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.part.ViewPart;
+
+public class ViewGeneralMenu {
+	
+	
+	public ViewGeneralMenu(){
+		
+	}
+	
+	public void OpenDevicesAction() {
+
+	}
+
+}
